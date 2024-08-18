f411d6597ed2bc1ef99b62fb9d8893ee8303dfff
RaymondOnn <raymondonn@gmail.com>
2024-08-18 13:13:33 +0800

    chore: backup files

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from pprint import pprint
-
 from src.exceptions import handle_exception
 from src.pipeline.pipeline import Pipeline
-
 
 
 def main():
@@ -11,5 +8,17 @@ def main():
         pipeline.generate_dag().run("2023-04-31")
     except Exception as err:
         handle_exception(err, "Something went wrong")
+
+
+def hello():
+    print("hello")
+
+
+def test():
+    params = {"callback_func": hello}
+    print(params)
+
+
 if __name__ == "__main__":
     main()
+    # test()
--- tasks.md
+++ tasks.md
@@ -4,3 +4,7 @@
 - Schema Definition for normalize stage
 - Mode: Sequential / Parallel when there's branching
 - validation system
+
+- How to deal with outputs and inputs
+- Exception logging: clashing between error handler and decorator?
+- callback functions
